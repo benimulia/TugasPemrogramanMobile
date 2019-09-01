8c3f274b4a452fc63ce65ae2a184c406459441d3
benimulia <beni.mulia@si.ukdw.ac.id>
2019-09-01 19:39:12 +0700

    First commit

---
+++ app/src/main/java/com/example/proteintracker/HelpActivity.java
@@ -0,0 +1,25 @@
+package com.example.proteintracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*TextView myTextView = new TextView(this);
+        myTextView.setText("Some Help Text");
+        setContentView(myTextView);*/
+
+        TextView myTextView = new TextView(this);
+
+        Bundle b = getIntent().getExtras();
+
+        String helpText = b.getString("helpString");
+        myTextView.setText(helpText);
+        setContentView(myTextView);
+    }
+}
