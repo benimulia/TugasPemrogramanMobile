2cb814448bb7c29cf0feb8cda11e62c70389a5b5
benimulia <beni.mulia@si.ukdw.ac.id>
2019-11-06 19:00:55 +0700

    Menambahkan Preferences / Setting

--- app/src/main/java/com/example/proteintracker/ProteinTracker.java
+++ app/src/main/java/com/example/proteintracker/ProteinTracker.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,19 @@ public class ProteinTracker extends AppCompatActivity {
 
         Button resetButton = (Button)findViewById(R.id.btnReset);
         resetButton.setOnClickListener(resetButtonListener);
+
+        Button settingButton = (Button)findViewById(R.id.btnSettingProTrack);
+        settingButton.setOnClickListener(settingButtonListener);
+
+        SharedPreferences prefs = ProteinTracker.this.getSharedPreferences("prefs_file",MODE_PRIVATE);
+        String statusLogin = prefs.getString("isLogin",null); Button btnLogin = (Button)findViewById(R.id.btnLoginProTrack);
+        btnLogin.setOnClickListener(myBtnLoginClick);
+
+        if (statusLogin != null){
+            btnLogin.setText("Logout");
+        }else{
+            btnLogin.setText("Login");
+        }
     }
 
     private View.OnClickListener resetButtonListener = new View.OnClickListener(){
@@ -35,6 +49,34 @@ public class ProteinTracker extends AppCompatActivity {
                             Toast.makeText(ProteinTracker.this, "Melakukan RESET !!", Toast.LENGTH_SHORT).show();}         });
 
             AlertDialog dialog = builder.create(); dialog.show();
+        }
+    };
+
+    private View.OnClickListener settingButtonListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            Intent intent = new Intent(ProteinTracker.this, SettingProteinTracker.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener myBtnLoginClick  = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            SharedPreferences prefs = ProteinTracker.this.getSharedPreferences("prefs_file",MODE_PRIVATE);
+
+            String statusLogin = prefs.getString("isLogin",null);
+            SharedPreferences.Editor edit = prefs.edit();
+
+            Button btnLogin = (Button)findViewById(R.id.btnLoginProTrack);
+            if (statusLogin != null){
+                edit.putString("isLogin",null);
+                btnLogin.setText("Login");
+            }else{
+                edit.putString("isLogin","Admin");
+                btnLogin.setText("Logout");
+            }
+                edit.commit();
         }
     };
 }
---
+++ app/src/main/java/com/example/proteintracker/SettingProteinTracker.java
@@ -0,0 +1,44 @@
+package com.example.proteintracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingProteinTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            startActivity(new Intent(this, SettingProteinTracker.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
